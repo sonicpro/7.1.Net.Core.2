1034e47d7c7443af1e415fc45ac05c61fe551fd1
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2018-11-01 22:06:37 +0200

    Some demos from Chapters 1, 7, 9, 10.

---
+++ Code/Chapter09/WorkingWithSerialization/WorkingWithSerialization/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+namespace Packt.CS7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>
+            {
+                new Person(30000M) { FirstName = "Alice", LastName = "Smith", DateOfBirth = new DateTime(1974, 3, 14) },
+                new Person(40000M) { FirstName = "Bob", LastName = "Jones", DateOfBirth = new DateTime(1969, 11, 23) },
+                new Person(20000.12M)
+                {
+                    FirstName = "Charlie",
+                    LastName = "Rose", DateOfBirth = new DateTime(1964, 5, 4),
+                    Children = new HashSet<Person> { new Person(0M) { FirstName = "Sally", LastName = "Rose", DateOfBirth = new DateTime(1990, 7, 12) } }
+                }
+            };
+
+            string path = Combine(CurrentDirectory, "people.xml");
+
+            FileStream stream = File.Create(path);
+
+            // XML formatter for List<Person> objects to XML and for getting and object with the List<Person> structure back.
+            var xs = new XmlSerializer(typeof(List<Person>));
+            xs.Serialize(stream, people);
+            stream.Close();
+
+            WriteLine($"Written {new FileInfo(path).Length} bytes of XML to {path}");
+            WriteLine();
+
+            // Display the serialized object graph.
+            WriteLine(File.ReadAllText(path));
+
+            FileStream xmlLoad = File.Open(path, FileMode.Open);
+
+            var loadedPeople = (List<Person>)xs.Deserialize(xmlLoad);
+
+            foreach(Person p in loadedPeople)
+            {
+                WriteLine($"{p.LastName} has {p.Children.Count} children.");
+            }
+
+            if (loadedPeople.All(p => p.HasZeroSalary))
+            {
+                WriteLine("Salary data is totally lost during the serialization");
+            }
+        }
+    }
+}
---
+++ Code/Chapter10/CryptographyLib/Protector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Packt.CS7
+{
+	public class Protector
+	{
+		// Prepare salt and iteration count for IV and Key byte arrays generator.
+		private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS"); // 16-byte long salt.
+		private static readonly int iterations = 2000;
+
+		// Encrypt symmetrically.
+		public static string Encrypt(string plainText, string password)
+		{
+			byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+			var aes = Aes.Create();
+
+			// Create "byte array generator"
+			Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+			aes.Key = pbkdf2.GetBytes(32); // 256 (32*8)-bit key.
+			aes.IV = pbkdf2.GetBytes(16);  // 128-bit IV
+			var ms = new MemoryStream();
+			using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+			{
+				cs.Write(plainBytes, 0, plainBytes.Length);
+			}
+			return Convert.ToBase64String(ms.ToArray());
+		}
+
+		public static string Decrypt(string cryptoText, string password)
+		{
+			byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+			var aes = Aes.Create();
+			var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+			aes.Key = pbkdf2.GetBytes(32);
+			aes.IV = pbkdf2.GetBytes(16);
+			var ms = new MemoryStream();
+			using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+			{
+				cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+			}
+			return Encoding.Unicode.GetString(ms.ToArray());
+		}
+
+	}
+}
