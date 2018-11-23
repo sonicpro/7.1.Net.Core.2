24877d39b0b0fc13bb46ee0a59723f0f15cf17cc
sonicpro <sonic@ukrpost.net>
2018-11-23 14:09:45 +0200

    SwaggerGen and SwaggerUI ASP.NET Core package added.

--- Code/Part3/NorthwindService/Startup.cs
+++ Code/Part3/NorthwindService/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 using NorthwindService.Repositories;
 using Packt.CS7;
 
@@ -24,6 +25,11 @@ namespace NorthwindService
 				options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=True;MultipleActiveResultSets=true;"));
 			services.AddMvc();
 			services.AddScoped<ICustomerRepository, CustomerRepository>();
+			services.AddSwaggerGen(options => options.SwaggerDoc("v1", new Info
+			{
+				Title = "Northwind Service API",
+				Version = "v1"
+			}));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,9 @@ namespace NorthwindService
 			}
 
 			app.UseMvc();
+			app.UseSwagger();
+			// Define an endpoint where the JSON document in OpenAPI format can be downloaded.
+			app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind Service API V1"));
 		}
 	}
 }
